c753e966f520cb662bc59f7b12aa078185a0844b
LyraDusk <lauren@meyerzone.net>
2019-04-24 13:56:45 -0400

    it runs now

---
+++ 212Final/src/MazeGen.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.awt.geom.Point2D;
+
+public class MazeGen {
+	
+	
+	public MazeGen(int w, int h) {
+		
+		int WIDTH = w;
+		
+		int HEIGHT = h;
+		
+		List<Point2D> visited = new ArrayList<>();
+		
+		Random rand = new Random();
+	}
+
+	public static void main(String[] args) {
+		//Use PlayFish as a template for the world drawing
+		System.out.println("Test");
+		
+	}
+
+}
